38d6a955d5987e0576a633df3ff5d14046366816
Pres121 <preschiko@gmail.com>
2026-04-17 03:16:45 +0200

    Fix Netlify API path normalization

--- OneDrive/Desktop/Netflix software/netlify/functions/api.js
+++ OneDrive/Desktop/Netflix software/netlify/functions/api.js
@@ -1,4 +1,17 @@
+const express = require('express');
 const serverless = require('serverless-http');
 const app = require('../../server');
 
-module.exports.handler = serverless(app);
+const netlifyApp = express();
+
+netlifyApp.use((req, res, next) => {
+	if (!req.url.startsWith('/api')) {
+		req.url = `/api${req.url === '/' ? '' : req.url}`;
+	}
+
+	next();
+});
+
+netlifyApp.use(app);
+
+module.exports.handler = serverless(netlifyApp);
