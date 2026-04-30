720fa09778c9b3f254ccaccf6c7453bb27b68cae
Pres121 <preschiko@gmail.com>
2026-04-30 14:40:25 +0200

    Fix: 404 API error and login form alignment

--- OneDrive/Desktop/Netflix software/netlify/functions/api.js
+++ OneDrive/Desktop/Netflix software/netlify/functions/api.js
@@ -4,14 +4,6 @@ const app = require('../../server');
 
 const netlifyApp = express();
 
-netlifyApp.use((req, res, next) => {
-	if (!req.url.startsWith('/api')) {
-		req.url = `/api${req.url === '/' ? '' : req.url}`;
-	}
-
-	next();
-});
-
 netlifyApp.use(app);
 
 module.exports.handler = serverless(netlifyApp);
