375fbe2ed34f8d06447764bf75236b7977397347
Pres121 <preschiko@gmail.com>
2026-04-30 14:56:05 +0200

    Remove incorrect nested OneDrive directory structure

--- OneDrive/Desktop/Netflix software/netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = require('../../server');
-
-const netlifyApp = express();
-
-netlifyApp.use(app);
-
-module.exports.handler = serverless(netlifyApp);
